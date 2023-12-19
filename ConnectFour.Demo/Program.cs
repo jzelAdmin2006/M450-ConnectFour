963460139054001cdde50b94d21d15be56c7cdc7
Patrick Bucher <patrick.bucher@sluz.ch>
2023-12-19 19:29:24 +0100

    playable but buggy version of ConnectFour

--- ConnectFour.Demo/Program.cs
+++ ConnectFour.Demo/Program.cs
@@ -4,7 +4,26 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("ConnectFour");
-        new Board().Output();
+        var board = new Board();
+        var finished = false;
+        var player = Board.PLAYER_1;
+
+        board.Output();
+
+        while (!finished)
+        {
+            Console.Write($"Player {player}: ");
+            var input = Console.ReadLine().Trim();
+            var column = int.Parse(input);
+            var row = board.MakeMove(player, column);
+            board.Output();
+            var winner = board.Winner(player, row, column);
+            if (winner != Board.EMPTY)
+            {
+                Console.WriteLine($"Player {player}: A winner is you!");
+                break;
+            }
+            player = (player == Board.PLAYER_1) ? Board.PLAYER_2 : Board.PLAYER_1;
+        }
     }
 }
--- ConnectFour/Board.cs
+++ ConnectFour/Board.cs
@@ -4,7 +4,7 @@ public class Board
 {
     public const char PLAYER_1 = 'x';
     public const char PLAYER_2 = 'o';
-    private const char EMPTY = '_';
+    public const char EMPTY = '_';
     private const int ROWS = 6;
     private const int COLS = 7;
     public Board()
@@ -25,9 +25,9 @@ public class Board
     {
         for (var c = 0; c < fields[0].Length; c++)
         {
-            Console.Write($"{c+1} ");
-            Console.WriteLine();
+            Console.Write($"{c} ");
         }
+        Console.WriteLine();
         for (var r = 0; r < fields.Length; r++)
         {
             for (var c = 0; c < fields[r].Length; c++)
@@ -36,5 +36,120 @@ public class Board
             }
             Console.WriteLine();
         }
+    }
+
+    public int MakeMove(char player, int column)
+    {
+        for (var r = fields.Length - 1; r >= 0; r--)
+        {
+            if (fields[r][column] == EMPTY)
+            {
+                fields[r][column] = player;
+                return r;
+            }
+        }
+        return -1;
+    }
+
+    public char Winner(char player, int row, int col)
+    {
+        var horizontal = HorizontalWinner(player, row);
+        if (horizontal != EMPTY)
+        {
+            return horizontal;
+        }
+        var vertical = VerticalWinner(player, col);
+        if (vertical != EMPTY)
+        {
+            return vertical;
+        }
+        var diagonal = DiagonalWinner(player, row, col);
+        if (diagonal != EMPTY)
+        {
+            return diagonal;
+        }
+        return EMPTY;
+    }
+
+    private char HorizontalWinner(char player, int r)
+    {
+        char[] row = GetRow(r);
+        var line = new string(row);
+        var win = new string(player, 4);
+        if (line.Contains(win))
+        {
+            return player;
+        }
+        return EMPTY;
+    }
+    private char VerticalWinner(char player, int c)
+    {
+        char[] col = GetCol(c);
+        var line = new string(col);
+        var win = new string(player, 4);
+        if (line.Contains(win))
+        {
+            return player;
+        }
+        return EMPTY;
+    }
+
+    private char DiagonalWinner(char player, int r, int c)
+    {
+        var diagonals = GetDiagonals(r, c);
+        var diagUp = new string(diagonals[0]);
+        var diagDown = new string(diagonals[1]);
+        var win = new string(player, 4);
+        if (diagUp.Contains(win) || diagDown.Contains(win))
+        {
+            return player;
+        }
+        return EMPTY;
+    }
+
+    private char[] GetRow(int r)
+    {
+        var row = new char[COLS];
+        for (int i = 0; i < COLS; i++)
+        {
+            row[i] = fields[r][i];
+        }
+        return row;
+    }
+
+    private char[] GetCol(int c)
+    {
+        var col = new char[ROWS];
+        for (int i = 0; i < ROWS; i++)
+        {
+            col[i] = fields[i][c];
+        }
+        return col;
+    }
+
+    private char[][] GetDiagonals(int r, int c)
+    {
+        // Woe to thee, who entered here, for you dug too deep and unearthed
+        // daemons of the otherworld long-forgotten by mankind!
+        // https://youtu.be/TDIr9on8Rhw https://youtu.be/Mue6Vc_T9Ds
+        var raising = new List<char>();
+        var falling = new List<char>();
+        for (int i = r, j = c; i >= 0 && j < COLS; i--, j++)
+        {
+            raising.Add(fields[i][j]);
+        }
+        for (int i = r, j = c; i < ROWS && j >= 0; i++, j--)
+        {
+            raising.Add(fields[i][j]);
+        }
+        for (int i = r, j = c; i < ROWS && j < COLS; i++, j++)
+        {
+            falling.Add(fields[i][j]);
+        }
+        for (int i = r, j = c; i >= 0 && j >= 0; i--, j--)
+        {
+            falling.Add(fields[i][j]);
+        }
+        return new char[][] { raising.ToArray(), falling.ToArray() };
     }
 }
